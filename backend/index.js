b3ec739b8eb218585184bd3135dbb99050c16e3f
Amit Thakur <amitthakur2915@gmail.com>
2025-09-04 15:42:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://voice-assistant-app-j2pf.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
